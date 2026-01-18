8ae8b1b7b327c54df9cdb9b4d51891509968f714
Amanda Crittenden <49004058+akcrittenden@users.noreply.github.com>
2026-01-17 17:40:41 -0800

    trying to get spawning working

---
+++ Assets/ObjectSpawnerMine.cs
@@ -0,0 +1,36 @@
+
+using Oculus.Interaction;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+       {
+            SpawnObject();
+       }
+    }
+
+    public void SpawnObject()
+    {
+        GameObject furniture = Instantiate(prefabToSpawn, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch), OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch));
+        if (furniture != null)
+        {
+            Vector3 spawnPosition = transform.position + transform.forward * 0.5f; // Spawn 0.5 units in front of the spawner
+            Quaternion spawnRotation = Quaternion.identity; // No rotation
+            Instantiate(prefabToSpawn, spawnPosition, spawnRotation);
+        }
+        else
+        {
+            Debug.LogError("Prefab not found in Resources folder!");
+}
+    }
+}
