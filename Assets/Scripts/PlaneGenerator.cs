9cdb59f941a6901c6a4747d26e062bfc70842cc4
Amanda Crittenden <akcrittenden@gmail.com>
2026-04-23 20:27:29 -0700

    got trigger button to output to console again!! FOR FUTURE REFERENCE I need using UnityEngine.InputSystem; and using UnityEngine.InputSystem.XR; I'm using the XR INTERACTION TOOLKIT STUFF. I make a private INPUTACTIONREFERENCE and put my controller button in there, like XRI Right Interaction Activate for trigger. spawning and moving furniture still works!

--- Assets/Scripts/PlaneGenerator.cs
+++ Assets/Scripts/PlaneGenerator.cs
@@ -1,51 +1,74 @@
-using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
-using Oculus.Interaction.Input;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.ARFoundation;
 
-[RequireComponent(typeof(MeshFilter))]
+//[RequireComponent(typeof(MeshFilter))]
 public class PlaneGenerator : MonoBehaviour
 {
-    Vector3[] vertices;
-    int[] triangles;
+    //Vector3[] vertices;
+    //int[] triangles;
 
-    Mesh mesh;
-    [SerializeField] private XRDirectInteractor controller;
-    [SerializeField] private InputActionProperty pointButton;
-    [SerializeField] private GameObject pointObject;
+    //Mesh mesh;
+    [SerializeField] private InputActionReference triggerAction;
+
+    public XRController controller;
+    //[SerializeField] public InputActionReference pointButton;
+    //[SerializeField] public GameObject pointObject;
     // line renderer to connect points
 
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-
+        //mesh = new Mesh();
+        //GetComponent<MeshFilter>().mesh = mesh;
+        
     }
-
     private void Update()
     {
-        bool triggerValue = pointButton.action.ReadValue<bool>();
-        // when button pressed, call createpoint
-        if (pointButton.action.ReadValue<bool>())
+
+        if (triggerAction?.action == null)
         {
-            //CreatePoint();
-            Debug.Log(triggerValue);
+            Debug.LogWarning("Trigger action not assigned!");
+            return;
         }
 
-    }
+        // Method 1: Check if performed this frame
+        if (triggerAction.action.WasPerformedThisFrame())
+        {
+            Debug.Log("Trigger pressed!");
+            CreatePoint();
+        }
+
+        // Method 2: Check held state (binary)
+        if (triggerAction.action.IsPressed())
+        {
+            Debug.Log("Trigger held");
+        }
+
+        // Method 3: Get analog value (0-1)
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        if (triggerValue > 0.1f)
+        {
+            Debug.Log($"Trigger analog: {triggerValue}");
+        }
+
+        }
+        //bool triggerValue = pointButton.was;
+        //// when button pressed, call createpoint
+        //if (pointButton.action.ReadValue<bool>())
+        //{
+        //    //CreatePoint();
+        //    Debug.Log(triggerValue);
+        //}
+
 
     void CreatePoint()
     {
         // Does this need to be an anchor??
         // make point at controller front position and add to vertices list
         // get controller position
-        Vector3 controllerPosition = controller.transform.position;
+        //Vector3 controllerPosition = controller.transform.position;
         // instantiate point object at controller position
         // make first point a snapPoint
         // instantiate line renderer to first point
@@ -66,23 +89,24 @@ public class PlaneGenerator : MonoBehaviour
 
         //};
 
-        if (vertices.Length >= 3)
-        {
-            triangles = new int[]
-            {
-            0, 1, 2
-            };
-        }
+        //    if (vertices.Length >= 3)
+        //    {
+        //        triangles = new int[]
+        //        {
+        //        0, 1, 2
+        //        };
+        //    }
 
-    }
+        //}
 
-    void UpdateMesh()
-    {
-        mesh.Clear();
+        //void UpdateMesh()
+        //{
+        //    mesh.Clear();
 
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        //    mesh.vertices = vertices;
+        //    mesh.triangles = triangles;
 
-        mesh.RecalculateNormals();
+        //    mesh.RecalculateNormals();
+        //}
     }
 }
