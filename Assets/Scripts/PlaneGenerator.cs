75cf39389c9bd10ffeb7835ba8e69e4a77ef07c6
Amanda Crittenden <akcrittenden@gmail.com>
2026-04-25 16:44:06 -0700

    Added check if ray is over UI then don't spawn point

--- Assets/Scripts/PlaneGenerator.cs
+++ Assets/Scripts/PlaneGenerator.cs
@@ -6,28 +6,30 @@ using UnityEngine.InputSystem.XR;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-//[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(XRController))]
+
 public class PlaneGenerator : MonoBehaviour
 {
-    //Vector3[] vertices;
-    //int[] triangles;
-    //public XRController controller;
     public XRDirectInteractor controller;
+    public XRRayInteractor ray;
     protected Vector3 controllerPosition;
     protected Quaternion controllerRotation;
-    //Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    Mesh mesh; 
+
     [SerializeField] private InputActionReference triggerAction;
     [SerializeField] private GameObject pointPrefab;
 
-    //[SerializeField] public InputActionReference pointButton;
-    //[SerializeField] public GameObject pointObject;
+
     // line renderer to connect points
 
     void Start()
     {
-        //mesh = new Mesh()
-        //GetComponent<MeshFilter>().mesh = mesh;
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
 
     }
     private void Update()
@@ -42,12 +44,13 @@ public class PlaneGenerator : MonoBehaviour
         // Method 1: Check if performed this frame
         if (triggerAction.action.WasPerformedThisFrame())
         {
-            Debug.Log("Trigger pressed! Getting position...");
-            CreatePoint();
+            if (!ray.IsOverUIGameObject())
+            {
+                Debug.Log("Trigger pressed! Getting position...");
+                CreatePoint();
+            }
         }
-
-         
-        }
+    }
         //bool triggerValue = pointButton.was;
         //// when button pressed, call createpoint
         //if (pointButton.action.ReadValue<bool>())
@@ -61,14 +64,10 @@ public class PlaneGenerator : MonoBehaviour
     {
         controllerPosition = controller.transform.position;
         controllerRotation = controller.transform.rotation;
-        Debug.Log("Controller Position: " + controllerPosition);
-        // Does this need to be an anchor??
-        GameObject prefab = Instantiate(pointPrefab, controllerPosition, controllerRotation);
+        GameObject point = Instantiate(pointPrefab, controllerPosition, controllerRotation);
         Debug.Log("Sphere created at: " + controllerPosition);
         // make point at controller front position and add to vertices list
-        // get controller position
-        //Vector3 controllerPosition = controller.transform.position;
-        // instantiate point object at controller position
+
         // make first point a snapPoint
         // instantiate line renderer to first point
         // maybe keep track of which point came first? or instantiate a new one
